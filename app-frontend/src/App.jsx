82b59b13f68bb9925bf653785e982c1751e68e3d
EJHacks <ee1912136@gmail.com>
2026-01-02 17:22:10 -0500

    fixed css import

--- app-frontend/src/App.jsx
+++ app-frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import Sidebar from "./components/Sidebar.jsx";
 import SettingsPanel from "./components/SettingsPanel.jsx";
 import LibraryView from "./components/LibraryView.jsx";
-import CSS from "./styles/index.css";
+import "./styles/index.css";
+
 
 export default function App() {
   return (
